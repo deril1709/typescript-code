188473fcbcc76167c3ae7b3e9c7dc313eb56ad71
deril1709 <punktator17@gmail.com>
2023-07-19 20:54:36 +0700

    data type

---
+++ dist/test/data-type.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe('data-type', function () {
+    it('should be', function () {
+        const name = "Theo";
+        var angka = 10000;
+        let isVIP = true;
+        console.log(name);
+        console.log(angka);
+        console.log(isVIP);
+    });
+});
---
+++ test/data-type.test.ts
@@ -0,0 +1,14 @@
+describe ('data-type', function() {
+    it('should be', function(){
+
+        const name : string = "Theo";
+        var angka : number = 10000;
+        let isVIP : boolean = true;
+        
+        console.log(name)
+        console.log(angka)
+        console.log(isVIP)
+
+    });
+        
+});
