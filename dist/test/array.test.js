3647f38bbfd6a7daa47fd639f1dbbb5667d432b3
deril1709 <punktator17@gmail.com>
2023-07-20 21:20:31 +0700

    data-type

---
+++ dist/test/array.test.js
@@ -0,0 +1,18 @@
+"use strict";
+describe('array', function () {
+    it('should', function () {
+        const names = ['yoru', 'phoenix'];
+        const values = [1, 2, 3];
+        names[0] = 'ace';
+        console.info(names);
+        console.info(values);
+    });
+    //read only
+    it('read-only', function () {
+        const secondName = ['sage', 'omen'];
+    });
+    //tuple
+    it('', function () {
+        const person = ['reyna', 'skye', 60];
+    });
+});
